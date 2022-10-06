1c54b7fc124fc8c52d72c9d04f00bf837a9b7383
JinJu1124 <banggi1124@gmail.com>
2022-10-06 19:21:53 +0900

    [Feat] Link 연결

--- src/page/DiaryCreate.js
+++ src/page/DiaryCreate.js
@@ -1,8 +1,18 @@
 import React from 'react'
+import Button from 'react-bootstrap/Button';
+import Navigation from '../component/Navigation'
+import { Routes, Route ,Link} from "react-router-dom";
 
 const DiaryCreate = () => {
   return (
-    <h1>일기를 작성하고 저장하는 페이지 (DiaryCreate)</h1>
+    <div>
+      <Navigation/>
+      <h1>일기를 작성하고 저장하는 페이지 (DiaryCreate)</h1>
+      <Link to="/diary-detail"><Button variant="info">저장</Button></Link>
+      <Button variant="info">임시저장</Button>
+      <Link to="/diary-list"><Button variant="info">취소</Button></Link>
+    </div>
+
   )
 }
 
--- src/page/Mypage.js
+++ src/page/Mypage.js
@@ -1,8 +1,12 @@
 import React from 'react'
+import Navigation from '../component/Navigation'
+import { Routes, Route ,Link} from "react-router-dom";
 
 const Mypage = () => {
   return (
+    <div><Navigation/>
     <h1>마이페이지 (Mypage)</h1>
+    </div>
   )
 }
 
