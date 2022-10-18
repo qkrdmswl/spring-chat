9707bfb2fcb9886ab8fd5fa5dcac6831fb647826
CHAN HEE LEE <95417144+CHANHELEE@users.noreply.github.com>
2022-10-18 19:22:13 +0900

    [Feat] simple jwt change

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,9 @@
 import {configureStore,getDefaultMiddleware} from "@reduxjs/toolkit"
 import musicReducer from "./reducers/musicReducer"
-import userReducer from'./reducers/userReducer'
 
 const store=configureStore({
   reducer:{
-    user:userReducer,
+
   },
   middleware: getDefaultMiddleware({
     serializableCheck: false,
