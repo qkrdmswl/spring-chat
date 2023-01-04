6396d37c5ab898b1b2e1f2bf919136e842cec95d
EUNJI-PARK <65437087+qkrdmswl@users.noreply.github.com>
2023-01-04 15:36:00 +0900

    Delete nlp_django_server directory

--- nlp_django_server/nlpproject/api/urls.py
+++
@@ -1,8 +0,0 @@
-from . import views
-from django.urls import path, include
-
-urlpatterns = [
-    path('create/', views.CreateTagView.as_view()),
-    path('update/<int:pk>', views.UpdateTagView.as_view()),
-    path('delete/<int:pk>', views.DeleteTagView.as_view()),
-]
