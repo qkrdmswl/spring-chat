35859b931f3e826a85f799b50f8e2e91b0be51a0
qkrdmswl <eunji303@naver.com>
2023-01-06 15:28:25 +0900

    initial commit

--- nlp_node_server/routes/v1/index.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-
-const router = express.Router();
-
-const postRouter = require("./post/postRouter");
-
-router.use("/post", postRouter);
-
-module.exports = router;
