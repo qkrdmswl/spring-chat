10773bd581eb232aeb593d193323bc6f9a90b4ea
qkrdmswl <eunji303@naver.com>
2023-02-01 15:42:42 +0900

    test: now/pending test done

--- nlp_flask_server/config/db.py
+++ nlp_flask_server/config/db.py
@@ -1,7 +1,6 @@
 import os
 import pymysql
 
-
 # configuration for development DB
 class dev_db():
 
@@ -10,32 +9,28 @@ class dev_db():
             host=os.getenv('DEV_DB_HOST'),
             user=os.getenv('DB_USER'),
             password=os.getenv('DB_PASSWORD'),
-            db='mysql',
-            port=os.getenv('DB_PORT')
+            db='shotag',
+            port=int(os.getenv('DB_PORT'))
         )
+        print("successfully connect to dev_db")
         self.cursor = self.db.cursor(pymysql.cursors.DictCursor)
 
     def get_nlp_status(self, imageIdx):
-        query = "SELECT NLP_STATUS FROM IMAGE WHERE IMAGE_IDX=%s;"
+        query = "SELECT NLP_STATUS FROM IMAGE WHERE IMAGE_IDX=%s"
         self.cursor.execute(query, imageIdx)
+        row = self.cursor.fetchone()
+        return row.get('NLP_STATUS')
 
     def insert_tag(self, tags):
         query = "INSERT INTO TAG (IMAGE_IDX, CONTENT, TYPE) VALUES (%s, %s, %s)"
         self.cursor.executemany(query, tags)
 
-    def execute_one(self, query):
-        self.cursor.execute(query)
-        row = self.cursor.fetchone()
-        return row
-
-    def execute_all(self, query):
-        self.cursor.execute(query)
-        row = self.cursor.fetchall()
-        return row
+    def change_status(self, imageIdx):
+        query = "UPDATE IMAGE SET NLP_STATUS=1 WHERE IMAGE_IDX=%s"
+        return self.cursor.execute(query, imageIdx)
 
     def commit(self):
         self.db.commit()
-
 
 
 # configuration for production DB
@@ -46,28 +41,25 @@ class prod_db():
             host=os.getenv('DB_HOST'),
             user=os.getenv('DB_USER'),
             password=os.getenv('DB_PASSWORD'),
-            db='mysql',
-            port=os.getenv('DB_PORT')
+            db='shotag',
+            port=int(os.getenv('DB_PORT'))
         )
+        print("successfully connect to prod_db")
         self.cursor = self.db.cursor(pymysql.cursors.DictCursor)
 
     def get_nlp_status(self, imageIdx):
-        query = "SELECT NLP_STATUS FROM IMAGE WHERE IMAGE_IDX=%s;"
+        query = "SELECT NLP_STATUS FROM IMAGE WHERE IMAGE_IDX=%s"
         self.cursor.execute(query, imageIdx)
+        row = self.cursor.fetchone()
+        return row.get('NLP_STATUS')
 
     def insert_tag(self, tags):
         query = "INSERT INTO TAG (IMAGE_IDX, CONTENT, TYPE) VALUES (%s, %s, %s)"
         self.cursor.executemany(query, tags)
 
-    def execute_one(self, query):
-        self.cursor.execute(query)
-        row = self.cursor.fetchone()
-        return row
-
-    def execute_all(self, query):
-        self.cursor.execute(query)
-        row = self.cursor.fetchall()
-        return row
+    def change_status(self, imageIdx):
+        query = "UPDATE IMAGE SET NLP_STATUS=1 WHERE IMAGE_IDX=%s"
+        return self.cursor.execute(query, imageIdx)
 
     def commit(self):
         self.db.commit()
--- nlp_flask_server/subprocessor.py
+++ nlp_flask_server/subprocessor.py
@@ -1,9 +1,10 @@
 from sqs_prod.sqs_sender import sqsSender
 from sqs_prod.sqs_receiver import sqsReceiver
 from nlp_prod.nlp_main import NlpProcessor
-from api import model
+from nlp_prod.nlp_init import NlpInit
 from config.db import dev_db, prod_db
 import argparse
+
 
 
 
@@ -15,38 +16,50 @@ class subprocessor():
         self.sqsReceiver = sqsReceiver()
         self.db = db
 
-    def save_tag(self, model, ocrText, imageIdx):
+    def save_tag(self, ocrText, imageIdx):
         try:
-            nlp_status = db.get_nlp_status(imageIdx)
+            nlp_status = self.db.get_nlp_status(imageIdx)
+            print("****", nlp_status)
         except Exception as e:
             print(e)
             return False
 
         if nlp_status == 0:
-            processor = NlpProcessor(ocrText, model)
+            processor = NlpProcessor(ocrText, self.model)
+            print("**made processor")
+            processor.post_processing()
             tag_list = processor.NLP_Noun(score=True, ner=True)
-
+            print(tag_list)
             tags = []
             for i in range(len(tag_list)):
                 tags.append([imageIdx, tag_list[i][0], 1])
 
-            db.insert_tag(tags)
-            db.commit()
+            print(tags)
+            self.db.insert_tag(tags)
+            print("**insert tag")
+            self.db.commit()
+            self.db.change_status(imageIdx)
+            print("**change_status to 1")
+            self.db.commit()
+            print("**commit done")
 
 
 
     def process(self):
+        print("**start process...")
         while True:
-            print('new try!!!')
-            response = sqsReceiver.receive()
+            print('**new try!!!')
+            response = self.sqsReceiver.receive()
 
             if response == None:
+                print("**no response")
                 continue
 
             for data in response:
-                if self.save_tag(model, data[0], data[1]):
-                    sqsSender.push(data[1])
-
+                if self.save_tag(data[0], data[1]):
+                    print("**saving tags...")
+                    self.sqsSender.push(data[1])
+                    print("**push")
 
 
 # custom execute command
@@ -59,9 +72,12 @@ args = parser.parse_args()
 if args.exec == 'prod':
     db = prod_db()
 else:
+    print("**start dev server...")
     db = dev_db()
 
 
 # start the process
+model = NlpInit()
+print(model)
 processor = subprocessor(model, db)
 processor.process()
